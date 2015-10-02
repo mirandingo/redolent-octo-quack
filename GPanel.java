681cd6ddb8768284e36f0a29911322901b16a82b
mirandingo <nicolas.miranda98@gmail.com>
2015-10-02 13:57:13 -0400

    Create GPanel.java

---
+++ GPanel.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import javax.swing.*:
+
+public class GPanel extends JFrame {
+	private int duration;
+	JFrame window;
+
+
+	public Gpanel(int g){
+		duration =d;
+		window = new JFrame("PacMan");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+	public void showSplash(){
+		JPanel content =(Jpanel)getContentPanel();
+		content.setBackground(Color.white);
+
+		int width=600;
+		int height=800;
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
+		int x=0;
+		int y=0;
+		setBounds(x,y,widht,height);
+
+		
+}		
+}
+
